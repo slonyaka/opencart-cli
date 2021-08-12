8cd20438dee2af66769e357b75973719e30fccdf
s.slonchakov <segey.slonchakov@centum-d.com>
2021-08-12 09:07:42 +0300

    init

---
+++ src/Core/Kernel.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Kernel
+ * @author sergey.slonchakov
+ */
+
+namespace Slonyaka\OpencartCli\Core;
+
+class Kernel
+{
+
+    public function __construct()
+    {
+
+    }
+
+    public function handle()
+    {
+        echo 'hello';
+    }
+}
